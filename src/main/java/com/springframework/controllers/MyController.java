68d65f138942c6ad7dd50c98b4050b871e7365e1
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2020-06-20 13:24:08 +0300

    adding Primary Bean demo

--- src/main/java/com/springframework/controllers/MyController.java
+++ src/main/java/com/springframework/controllers/MyController.java
@@ -1,13 +1,20 @@
 package com.springframework.controllers;
 
+import com.springframework.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello(){
         System.out.println("Hello!!! ");
 
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }
--- src/main/java/com/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/com/springframework/controllers/PropertyInjectedController.java
@@ -1,14 +1,15 @@
 package com.springframework.controllers;
 
 import com.springframework.services.GreetingService;
-import com.springframework.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingService greetingServiceImpl;
 
     public String sayHello(){
