4d30f85bc01eb508d3135e7729fcc4ac7d653828
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2020-06-20 13:43:08 +0300

    adding default profile demo

---
+++ src/main/java/com/springframework/services/GreetingRepository.java
@@ -0,0 +1,10 @@
+package com.springframework.services;
+
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+
+    String getSpanishGreeting();
+
+    String getGermanGreeting();
+}
